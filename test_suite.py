269df60aaccc2f54b37c175e86247d1ceb323981
Victoria Nunez <vcnunez@usc.edu>
2023-11-14 11:10:06 -0800

    Update test_suite.py

--- test_suite.py
+++ test_suite.py
@@ -31,10 +31,10 @@ def read_light_sensor():
 def read_sound_sensor():
     raw_value = mcp.read_adc(1)  # Read from channel 1 (sound sensor)
     # threshold_sound = 500
-    # print(f"Sound: {raw_value}")
+    print(f"Sound: {raw_value}")
     if raw_value > 600:
         light_on = True
-        # print(f"Threshold achieved: {raw_value}")
+        print(f"Threshold achieved: {raw_value}")
         # Turn on the LED for 100ms
         GPIO.output(11, GPIO.HIGH)
         time.sleep(0.1)
